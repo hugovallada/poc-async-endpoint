f5cb799761fceefd0944e2288759054850190a3c
Hugo <valladahugo@gmail.com>
2023-11-18 17:52:58 -0300

    Substitui a injeção via campo pela injeção via construtor

--- src/main/java/com/github/hugovallada/asyncendpoint/infra/adapters/in/GetCepController.kt
+++ src/main/java/com/github/hugovallada/asyncendpoint/infra/adapters/in/GetCepController.kt
@@ -4,22 +4,17 @@ import com.github.hugovallada.asyncendpoint.application.port.`in`.GetCepUseCase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/kotlin")
-class GetCepControllerCoroutine {
-
-    @Autowired
-    lateinit var getcep: GetCepUseCase
-
+class GetCepControllerCoroutine(private val getCep: GetCepUseCase) {
     @GetMapping("/{cep}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     fun get(@PathVariable cep: String) {
         CoroutineScope(Dispatchers.IO).launch {
-            getcep.execute(cep)
+            getCep.execute(cep)
         }
     }
 
